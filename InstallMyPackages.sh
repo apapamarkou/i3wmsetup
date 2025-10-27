644a6b25f92b2203b5773a02e7bd3bdc7890ac8a
Andrianos Papamarkou <papamarkoua@gmail.com>
2025-10-28 01:12:20 +0200

    packages

--- InstallMyPackages.sh
+++ InstallMyPackages.sh
@@ -9,17 +9,18 @@ FILES_REPO='nnn gvfs thunar thunar-archive-plugin thunar-media-tags-plugin thuna
 
 MAN_REPO='man-db man-pages man-pages-el '
 
-GUI_APPS_REPO='firefox okular obsidian qpwgraph '
+GUI_APPS_REPO='firefox okular obsidian qpwgraph keepassxc mousepad'
 GUI_APPS_AUR='visual-studio-code-bin naps2 '
 
-# TUI Applications pacman
 TUI_APPS_REPO='mame micro nvim '
-TUI_APPS+AUR='pacseek pyradio '
+TUI_APPS+AUR='pacseek-bin pyradio '
 
-CLI_TOOLS_REPO='fish figlet fastfetch rsync '
+CLI_TOOLS_REPO='fish figlet fastfetch rsync bc cmatrix '
 CLI_TOOLS_AUR='cava '
 
 # Multimedia
-MULTIMEDIA_REPO='mpv feh' 
+MULTIMEDIA_REPO='mpv feh ' 
 
+sudo pacman -S --needed --noconfirm $DESKTOP_REPO $THEME_REPO $FILES_REPO $MAN_REPO $GUI_APPS_REPO $TUI_APPS_REPO $CLI_TOOLS_REPO $MULTIMEDIA_REPO
+ysy -S --needed --noconfirm $DESKTOP_AUR $GUI_APPS_AUR $TUI_APPS_AUR $CLI_TOOLS_AUR
 
---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./git.sh
+./greek.sh
+./greet.sh
+./password-feedback.sh
+./update-grub.sh
+./yay.sh
+./wine.sh
+./pro-audio.sh
+./InstallMyPackages.sh

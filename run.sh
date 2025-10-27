1136a418a975643f810521d89375432e518883f8
Andrianos Papamarkou <papamarkoua@gmail.com>
2025-10-28 01:27:31 +0200

    run script

--- run.sh
+++ run.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-./git.sh
-./greek.sh
-./greet.sh
-./password-feedback.sh
-./update-grub.sh
-./yay.sh
-./wine.sh
-./pro-audio.sh
-./InstallMyPackages.sh
+# find the script path
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Setup git
+$SCRIPT_DIR/scripts/git.sh
+# enable Greek language support
+$SCRIPT_DIR/scripts/scripts/greek.sh
+# change display manager to TUI Greeter
+$SCRIPT_DIR/scripts/scripts/greet.sh
+# Enable password feedback
+$SCRIPT_DIR/scripts/scripts/password-feedback.sh
+# Make the update-grub command
+$SCRIPT_DIR/scripts/update-grub.sh
+# Install yay
+$SCRIPT_DIR/scripts/yay.sh
+# Install wine
+$SCRIPT_DIR/scripts/wine.sh
+# Install pro-audio support
+$SCRIPT_DIR/scripts/pro-audio.sh
+# Install all my packages
+$SCRIPT_DIR/scripts/InstallMyPackages.sh
+# Change shell to fish
+chsh -s /usr/bin/fish
+$SCRIPT_DIR/scripts/copy-dotfiles.sh

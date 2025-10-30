8812cd7036e42dc61fca09efdc70d32e42179789
Andrianos Papamarkou <papamarkoua@gmail.com>
2025-10-30 03:59:43 +0200

    misc

--- src/InstallMyPackages.sh
+++ src/InstallMyPackages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DESKTOP_REPO='autotiling polybar brightnessctl dex dunst lxsession jq network-manager-applet networkmanager xfce4-clipman-plugin power-profiles-daemon rofi ttf-jetbrains-mono-nerd calcurse htop power-profiles-daemon fzf bluetui dua-cli usbutils alacritty maim slop xdotool xorg-xdpyinfo xorg-xwininfo gpick '
-DESKTOP_AUR='aarchup nitrogen '
+DESKTOP_REPO='autotiling polybar arandr pavucontrol brightnessctl dex dunst lxsession jq network-manager-applet networkmanager xfce4-clipman-plugin power-profiles-daemon rofi ttf-jetbrains-mono-nerd calcurse htop btop power-profiles-daemon fzf bluetui dua-cli usbutils alacritty maim slop xdotool xorg-xdpyinfo xorg-xwininfo gpick '
+DESKTOP_AUR='aarchup '
 
 THEME_REPO='kvantum kvantum-theme-materia materia-gtk-theme lxappearance qt5ct qt6ct '
 
@@ -9,13 +9,13 @@ FILES_REPO='nnn gvfs thunar thunar-archive-plugin thunar-media-tags-plugin thuna
 
 MAN_REPO='man-db man-pages man-pages-el '
 
-GUI_APPS_REPO='firefox okular obsidian qpwgraph keepassxc mousepad '
+GUI_APPS_REPO='firefox okular obsidian qpwgraph keepassxc '
 GUI_APPS_AUR='visual-studio-code-bin naps2 onlyoffice-bin '
 
 TUI_APPS_REPO='mame micro nvim '
 TUI_APPS_AUR='pacseek-bin pyradio '
 
-CLI_TOOLS_REPO='fish figlet fastfetch rsync bc cmatrix inetutils plocate '
+CLI_TOOLS_REPO='fish figlet rsync bc cmatrix inetutils plocate '
 CLI_TOOLS_AUR='cava '
 
 # Multimedia
@@ -24,3 +24,4 @@ MULTIMEDIA_REPO='mpv feh '
 sudo pacman -S --needed --noconfirm $DESKTOP_REPO $THEME_REPO $FILES_REPO $MAN_REPO $GUI_APPS_REPO $TUI_APPS_REPO $CLI_TOOLS_REPO $MULTIMEDIA_REPO
 yay -S --needed --noconfirm $DESKTOP_AUR $GUI_APPS_AUR $TUI_APPS_AUR $CLI_TOOLS_AUR
 
+sudo pacman -R --noconfirm xterm
